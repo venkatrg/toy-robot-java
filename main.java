8b08f0e5f2017bcc3ef6a937a1737cc1194f7d57
venkatrg <51226633+venkatrg@users.noreply.github.com>
2019-05-31 07:34:09 +0000

    Add files via upload

---
+++ main.java
@@ -0,0 +1,43 @@
+package com.venkatrg.trb;
+
+import com.venkatrg.trb.exception.ToyRobotException;
+import com.venkatrg.trb.simulator.SquareBoard;
+import com.venkat.trb.simulator.ToyRobot;
+
+import java.io.Console;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Console console = System.console();
+
+        if (console == null) {
+            System.err.println("No console.");
+            System.exit(1);
+        }
+
+        SquareBoard squareBoard = new SquareBoard(4, 4);
+        ToyRobot robot = new ToyRobot();
+        Game game = new Game(squareBoard, robot);
+
+        System.out.println("Toy Robot Simulator");
+        System.out.println("Enter a command, Valid commands are:");
+        System.out.println("\'PLACE X,Y,NORTH|SOUTH|EAST|WEST\', MOVE, LEFT, RIGHT, REPORT or EXIT");
+
+        boolean keepRunning = true;
+        while (keepRunning) {
+            String inputString = console.readLine(": ");
+            if ("EXIT".equals(inputString)) {
+                keepRunning = false;
+            } else {
+                try {
+                    String outputVal = game.eval(inputString);
+                    System.out.println(outputVal);
+                } catch (ToyRobotException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+}
